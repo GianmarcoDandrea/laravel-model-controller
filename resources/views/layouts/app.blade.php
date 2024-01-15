75dac53351937e5099fb20df89ca85c7de742d2d
GianmarcoDandrea <gianm96@hotmail.it>
2024-01-15 15:19:20 +0100

    formated all blade.php documents

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,14 +10,16 @@
     {{-- Importare la parte di js e sass tramite vite --}}
     @vite('resources/js/app.js')
 </head>
+
 <body>
     @include('partials.header')
 
     <main>
         @yield('content')
     </main>
-    
+
 
     @include('partials.footer')
 </body>
+
 </html>
--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -7,13 +7,17 @@
             @foreach ($movies as $movie)
                 <div class="card" style="max-width: 540px;">
                     <div class="row g-0">
+
                         <div class="col-md-4">
                             <img src="{{ $movie->image }}" class="img-fluid rounded-start" alt="...">
                         </div>
+
                         <div class="col-md-8">
+
                             <div class="card-body p-1">
                                 <h4 class="card-title text-center">{{ $movie->title }}</h4>
                             </div>
+
                             <ul class="list-group list-group-flush m-0">
                                 <li class="list-group-item">
                                     <h5 class="m-0">
@@ -47,6 +51,5 @@
                 </div>
             @endforeach
         </div>
-
     </div>
 @endsection
